ddb3d21ad2b9075c1feb674e545c0b2628b1e7d3
Zac Kilburn <kilburn.zachary@gmail.com>
2024-01-09 23:45:15 -0500

    Initial commit

---
+++ BetterDoorWarning.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BetterDoorWarning
+{
+	[HarmonyPatch]
+	internal class BetterDoorWarningFunctions
+	{
+		private bool lastCheckEnemy = false;
+
+		[HarmonyPatch(typeof(EntranceTeleport), "Update")]
+		private static class Patch
+		{
+			private static BetterDoorWarningFunctions betterDoorWarningFunctions = new();
+
+			[HarmonyPrefix]
+			private static void Update(EntranceTeleport __instance)
+			{
+				__instance.enemyNearLastCheck = true;
+				if ((Object)__instance.triggerScript == (Object)null || !__instance.isEntranceToBuilding)
+					return;
+				if ((double)__instance.checkForEnemiesInterval <= 0.0)
+				{
+					if (!__instance.gotExitPoint)
+					{
+						if (!__instance.FindExitPoint())
+							return;
+						__instance.gotExitPoint = true;
+					}
+					else
+					{
+						__instance.checkForEnemiesInterval = 1f;
+						bool flag = false;
+						bool flag2 = false;
+						for (int index = 0; index < RoundManager.Instance.SpawnedEnemies.Count; ++index)
+						{
+							if ((double)Vector3.Distance(RoundManager.Instance.SpawnedEnemies[index].transform.position, __instance.exitPoint.transform.position) < 7.6999998092651367 && !RoundManager.Instance.SpawnedEnemies[index].isEnemyDead)
+							{
+								flag = true;
+								break;
+							}
+							if ((double)Vector3.Distance(RoundManager.Instance.SpawnedEnemies[index].transform.position, __instance.exitPoint.transform.position) < 7.6999998092651367 && RoundManager.Instance.SpawnedEnemies[index].isEnemyDead)
+							{
+								flag2 = true;
+								break;
+							}
+						}
+						if (flag && !betterDoorWarningFunctions.lastCheckEnemy)
+						{
+							betterDoorWarningFunctions.lastCheckEnemy = true;
+							__instance.triggerScript.hoverTip = "[Near activity detected - He Alive]";
+						}
+						else if (flag2 && !betterDoorWarningFunctions.lastCheckEnemy)
+						{
+							betterDoorWarningFunctions.lastCheckEnemy = true;
+							__instance.triggerScript.hoverTip = "[Near activity detected - He Dead]";
+						}
+						else
+						{
+							if (!__instance.enemyNearLastCheck)
+								return;
+							betterDoorWarningFunctions.lastCheckEnemy = false;
+							__instance.triggerScript.hoverTip = "Enter: [LMB]";
+						}
+					}
+				}
+				else
+					__instance.checkForEnemiesInterval -= Time.deltaTime;
+				return;
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# LethalCompanyBetterDoorWarning
+Improved Entrance Door Warning when an enemy is nearby the door. If the enemy is dead it tells you.
+
+# Changes
+- V1.0.0
+	- Initial Release 
+
+# Installation
+1. Install BepInEx
+2. Run game once with BepInEx installed to generate folders/files
+3. Drop the DLL inside of the BepInEx/plugins folder
+4. No further steps needed
+
+# Feedback
+- Feel free to leave feedback or requests at [my github](https://github.com/bozzobrain/LethalCompanyBetterDoorWarning).
+
+# Buy me a coffee
+[Buy me a coffee](https://www.buymeacoffee.com/bozzobrain)
